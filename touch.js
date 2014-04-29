3eb3d10940a7a273bdd8e51d51d802f4c32beff9
lostcoaster <fjy456@gmail.com>
2014-04-29 20:09:22 +0900

    leave a file with old name to remain compatible

---
+++ touch.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name       Twitch touches pokemon
+// @namespace  https://github.com/lostcoaster/twitch-touches-pokemon
+// @author     lostcoaster
+// @version    0.4
+// @description  A tool adding a touch overlay onto the stream of twitchplayspokemon.
+
+// this include string credits Twitch Plays Pokemon Chat Filter
+// @include    /^https?://(www|beta)\.twitch\.tv\/twitchplayspokemon.*$/
+
+// @updateURL  https://raw.githubusercontent.com/lostcoaster/twitch-touches-pokemon/master/touch.user.js
+// ==/UserScript==
+
+
+//This file is obsoleted and kept only for compatibility. The working file was renamed to touch.user.js. Thanks to MattiasBuelens's suggestion.
+//If you are using this, turn to https://raw.githubusercontent.com/lostcoaster/twitch-touches-pokemon/master/touch.user.js instead
+(function(){document.body.appendChild(document.createElement('script')).src='https://raw.githubusercontent.com/lostcoaster/twitch-touches-pokemon/master/touch.user.js';})();
