7a50ae45e9ebeadeba3b07780e401e773d0ffaa3
lostcoaster <fjy456@gmail.com>
2014-05-02 14:55:36 +0900

    update version number

--- touch.user.js
+++ touch.user.js
@@ -3,7 +3,7 @@
 // @namespace      https://github.com/lostcoaster/twitch-touches-pokemon
 // @author         lostcoaster
 // @author         MattiasBuelens
-// @version        1.3
+// @version        1.4
 // @description    A tool adding a touch overlay onto the stream of twitchplayspokemon.
 // @grant          unsafeWindow
 
